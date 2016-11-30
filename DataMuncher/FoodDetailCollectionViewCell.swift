3de0ddaf27efaf185c3e96493f1ce58933c7ab73
Joe Elliott <joe.elliott@joulebug.com>
2016-11-30 08:01:01 -0500

    Styling and animations for food detail view

--- DataMuncher/FoodDetailCollectionViewCell.swift
+++ DataMuncher/FoodDetailCollectionViewCell.swift
@@ -14,5 +14,11 @@ class FoodDetailCollectionViewCell : UICollectionViewCell {
     @IBOutlet weak var iconView: UIImageView!
     @IBOutlet weak var numberLabel: UILabel!
     @IBOutlet weak var detailTitleLabel: UILabel!
+    @IBOutlet weak var containingView: UIView!
+    
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var trailingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
 
 }
--- DataMuncher/FoodDetailViewController.swift
+++ DataMuncher/FoodDetailViewController.swift
@@ -34,6 +34,9 @@ class FoodDetailViewController: UIViewController, UICollectionViewDataSource,UIC
     
     let formatter:NumberFormatter = NumberFormatter()
     
+    let kvoContentOffset = "contentOffset"
+    var originalCellWidth = 0.0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,6 +54,8 @@ class FoodDetailViewController: UIViewController, UICollectionViewDataSource,UIC
         self.theCollectionView.contentInset = UIEdgeInsetsMake(0, flowLayout.minimumInteritemSpacing, 0, flowLayout.minimumInteritemSpacing)
         
         initializeDataModel()
+        
+        self.theCollectionView.addObserver(self, forKeyPath: kvoContentOffset, options: [NSKeyValueObservingOptions.new,NSKeyValueObservingOptions.old], context: nil)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -62,17 +67,28 @@ class FoodDetailViewController: UIViewController, UICollectionViewDataSource,UIC
         let spacing = flowLayout.minimumInteritemSpacing
         
         let newWidth = (viewWidth - spacing)/2.0
+        originalCellWidth = Double(newWidth)
         
         flowLayout.itemSize = CGSize(width: newWidth, height: newWidth)
+    }
+    
+    deinit {
+        self.theCollectionView.removeObserver(self, forKeyPath: kvoContentOffset)
     }
 
     func initializeDataModel() {
         
         if let food = foodItem {
             
-            nutritionData = [NutritionData(title: NSLocalizedString("Fat", comment: ""), image: UIImage(named:"fat_icon")!, data: food.fat, color:UIColor(hexString:"996633")),
+            nutritionData = [
                              NutritionData(title: NSLocalizedString("Calories", comment: ""), image: UIImage(named:"calories_icon")!, data: food.calories, color:UIColor(hexString:"3399ff")),
-                             NutritionData(title: NSLocalizedString("Cholesterol", comment: ""), image: UIImage(named:"cholesterol_icon")!, data: food.cholesterol, color:UIColor(hexString:"cccc00"))
+                             NutritionData(title: NSLocalizedString("Fat", comment: ""), image: UIImage(named:"fat_icon")!, data: food.fat, color:UIColor(hexString:"996633")),
+                             NutritionData(title: NSLocalizedString("Protein", comment: ""), image: UIImage(named:"protein_icon")!, data: food.protein, color:UIColor(hexString:"b30000")),
+                             NutritionData(title: NSLocalizedString("Carbohydrates", comment: ""), image: UIImage(named:"carbohydrates_icon")!, data: food.carbohydrates, color:UIColor(hexString:"cc9900")),
+                             NutritionData(title: NSLocalizedString("Cholesterol", comment: ""), image: UIImage(named:"cholesterol_icon")!, data: food.cholesterol, color:UIColor(hexString:"cccc00")),
+                            NutritionData(title: NSLocalizedString("Sodium", comment: ""), image: UIImage(named:"sodium_icon")!, data: food.sodium, color:UIColor(hexString:"b3b3b3")),
+                            NutritionData(title: NSLocalizedString("Potassium", comment: ""), image: UIImage(named:"potassium_icon")!, data: food.potassium, color:UIColor(hexString:"ff9933")),
+                            NutritionData(title: NSLocalizedString("Fiber", comment: ""), image: UIImage(named:"fiber_icon")!, data: food.fiber, color:UIColor(hexString:"336600"))
             ]
         }
     }
@@ -92,7 +108,7 @@ class FoodDetailViewController: UIViewController, UICollectionViewDataSource,UIC
         var cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FoodDetailCollectionViewCell", for: indexPath)
         if let collectionCell = cell as? FoodDetailCollectionViewCell {
             let dataItem = nutritionData[indexPath.row]
-            collectionCell.contentView.backgroundColor = dataItem.color
+            collectionCell.containingView.backgroundColor = dataItem.color.withAlphaComponent(0.5)
             collectionCell.detailTitleLabel.text = dataItem.title
             collectionCell.iconView.image = dataItem.image
             collectionCell.numberLabel.text = formatter.string(from: NSNumber(value:dataItem.data))
@@ -101,6 +117,38 @@ class FoodDetailViewController: UIViewController, UICollectionViewDataSource,UIC
         }
         return cell
     }
+    
+    
+    //MARK: KVO Animations
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if(keyPath == kvoContentOffset){
+            if let value = change?[NSKeyValueChangeKey.newKey] as? CGPoint {
+                
+                if(value.y < 0){
+                    //we're pulling down on the scroll
+                    
+                    let visibleCells = self.theCollectionView.visibleCells
+                    
+                    for cell in visibleCells {
+                    
+                        if let detailCell = cell as? FoodDetailCollectionViewCell {
+                            UIView.animate(withDuration: 0.1, animations: {
+                                detailCell.bottomConstraint.constant = value.y
+                                detailCell.topConstraint.constant = value.y
+                                detailCell.leadingConstraint.constant = value.y
+                                detailCell.trailingConstraint.constant = value.y
+                                detailCell.layoutIfNeeded()
+                            })
+                        }
+                    }
+                }
+                
+                
+                
+            }
+        }
+    }
+    
 }
 
-    
+
